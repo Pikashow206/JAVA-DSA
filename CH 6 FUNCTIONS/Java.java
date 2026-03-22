8890491b38473d084f2b1f5b81b23785433fc621
Pikashow206 <piyush0kumar0@gmail.com>
2026-03-22 10:40:24 +0530

    Starting my Funcitons journey

---
+++ CH 6 FUNCTIONS/Java.java
@@ -0,0 +1,14 @@
+public class Java {
+    public static void add(int a , int b){
+        int sum = a + b;
+        System.out.println("The sum is : " + sum);
+         
+    }
+    public static void main(String[] args) {
+        
+        
+
+        
+        add(5, 10);
+    }
+}
